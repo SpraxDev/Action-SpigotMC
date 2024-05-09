c6fb41fad83d0d83dd4708fb2c53930821fac0e1
Christian Koop <contact@sprax2013.de>
2024-05-09 16:13:30 +0200

    feat(sftp-cache): Add support for uploading files using local scp binary
    Download is just very slow in GitHub Actions and I am not sure why...
    I'm just trying `scp` now in hopes that it handles the download better.
    
    Maybe I also need to limit to one down/upload at a time to prevent GitHub from
    throttling the connections or something? :|

--- src/cache/SFTPCache.ts
+++ src/cache/SFTPCache.ts
@@ -1,3 +1,6 @@
+import ChildProcess from 'node:child_process';
+import Fs from 'node:fs';
+import Os from 'node:os';
 import Path from 'node:path';
 
 import Ssh2SftpClient from 'ssh2-sftp-client';
@@ -5,11 +8,21 @@ import Ssh2SftpClient from 'ssh2-sftp-client';
 export default class SFTPCache {
   private static readonly CACHE_DIR = 'SpraxDev_Action-SpigotMC-Cache';
 
+  private readonly host: string;
+  private readonly port: number;
+  private readonly username: string;
+  private readonly privateKey: string;
+
   private readonly sftpClient = new Ssh2SftpClient();
   private readonly initPromise: Promise<void>;
 
   constructor(host: string, port: number, username: string, privateKey: string) {
-    this.initPromise = this.init(host, port, username, privateKey);
+    this.host = host;
+    this.port = port;
+    this.username = username;
+    this.privateKey = privateKey;
+
+    this.initPromise = this.init();
   }
 
   async shutdown(): Promise<void> {
@@ -34,7 +47,7 @@ export default class SFTPCache {
 
     const remotePath = SFTPCache.constructRemoteCacheFilePath(version);
     if (await this.sftpClient.exists(remotePath) === '-') {
-      await this.sftpClient.get(remotePath, destFilePath);
+      await this.downloadSftpFile(remotePath, destFilePath);
       return true;
     }
 
@@ -49,17 +62,61 @@ export default class SFTPCache {
     await this.sftpClient.put(filePath, remotePath);
   }
 
-  private async init(host: string, port: number, username: string, privateKey: string): Promise<void> {
+  private async init(): Promise<void> {
     await this.sftpClient.connect({
-      host,
-      port,
-      username,
-      privateKey
+      host: this.host,
+      port: this.port,
+      username: this.username,
+      privateKey: this.privateKey
     });
   }
 
   private async ensureInit(): Promise<void> {
     await this.initPromise;
+  }
+
+
+  private async downloadSftpFile(remotePath: string, destFilePath: string): Promise<void> {
+    if (!await this.doesScpBinaryExist()) {
+      await this.sftpClient.get(remotePath, destFilePath);
+      return;
+    }
+
+    const privateKeyTmpPath = await Fs.promises.mkdtemp(Path.join(Os.tmpdir(), '/'));
+    try {
+      await Fs.promises.writeFile(Path.join(privateKeyTmpPath, 'id'), this.privateKey, {mode: 0o600});
+      await new Promise<void>((resolve, reject) => {
+        const scpCommandArgs = [
+          '-B', // batch mode (prevents asking for passwords or passphrases)
+          '-i', Path.join(privateKeyTmpPath, 'id'),
+          this.constructScpRemoteUrl(remotePath),
+          Path.resolve(destFilePath)
+        ];
+        const process = ChildProcess.spawn('scp', scpCommandArgs, {stdio: 'inherit'});
+        process.on('error', reject);
+        process.on('exit', code => code === 0 ? resolve() : reject(new Error(`Exit code: ${code}`)));
+      });
+    } finally {
+      await Fs.promises.rm(privateKeyTmpPath, {recursive: true, force: true});
+    }
+  }
+
+  private async doesScpBinaryExist(): Promise<boolean> {
+    return new Promise((resolve) => {
+      const process = ChildProcess.spawn('scp', ['-B'], {timeout: 5000, stdio: 'ignore'});
+      process.on('error', () => resolve(false));
+      process.on('exit', code => resolve(code === 1));
+    });
+  }
+
+  private constructScpRemoteUrl(remotePath: string): string {
+    const url = new URL('scp://');
+    url.host = this.host;
+    url.port = this.port.toString();
+    url.pathname = remotePath;
+    url.username = this.username;
+
+    return url.toString();
   }
 
   private static constructRemoteCacheFilePath(version: string): string {
